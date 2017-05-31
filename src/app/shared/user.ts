0918dadd2b7c4cba48c1b3380c4ccb571d1d5e35
Michael <shamanoff@gmail.com>
2017-05-31 20:57:56 +0300

    home

---
+++ src/app/shared/user.ts
@@ -0,0 +1,14 @@
+export class User {
+
+  constructor(
+    public firstName = '',
+    public lastName = '',
+    public age = '',
+    public email = '',
+    public phone = '',
+    public street = '',
+    public city = '',
+  )
+  {}
+
+}
